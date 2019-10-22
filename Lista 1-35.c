e157be4c7cc0c46242191bf4f4ff8b473ebe0edd
Clênio Borges <clenioborgesx@gmail.com>
2019-10-22 18:07:42 -0300

    Update Lista 1-35.c

--- Lista 1-35.c
+++ Lista 1-35.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 void main (void){
-	int n, fat, i;
+	int n, fatorial, i;
 	
 	printf("Digite um numero inteiro e positivo:");
 	scanf("%d",&n);
-	fat=1;
-/*	i=n;
+	fatorial=1;
+	i=n;
 	while (i>0){
 		
-		fat= fat*i;
+		fatorial= fatorial*i;
 		
 		i--;
-	}*/
+	}
 	for(i=n;i>0;i--){
 		fat*=i;
 	}
 	
-	printf("O fatorial de %d eh %d.",n ,fat);
+	printf("O fatorial de %d eh %d.",n ,fatorial);
 	
 	
 }
+
